42691294eb7b36a3966c809a1f9ee3bb49680774
JKolokoltsev <j.kolokoltsev@gmail.com>
2017-03-15 00:19:36 -0600

    intervals, shooting... etc

--- src/examples/dist/distsim.cpp
+++ src/examples/dist/distsim.cpp
@@ -5,6 +5,7 @@
 #include <list>
 
 #include <boost/circular_buffer.hpp>
+#include <boost/math/special_functions/sign.hpp>
 #include <stdlib.h>
 
 using namespace std;
@@ -18,7 +19,7 @@ public:
 };
 
 const int lp = 2;
-const int lq = 2;
+const int lq = 1;
 constexpr int L = lp + lq;
 
 struct msg{
@@ -48,8 +49,28 @@ public:
 
 int main (){
 
-    for(int Sp = 10; Sp < 100; Sp++){
-        //int I = rand[Sp-L, Sp+L)....
+    for(int Sp = -100; Sp < 12345; Sp++){
+        //Arbitrary I from range in Qp
+        int I = Sp-L + (rand() % (2*L));
+
+        //calc modulus
+        int Sp_m = div(Sp,2*L).rem; //stored within process state
+        int I_m = div(I,2*L).rem; //received by message
+
+        //d_r - difference between modulus
+        int d_r = Sp_m-I_m;
+
+        //d_q === div(I,2*L).quot - div(Sp,2*L).quot; (difference between qoutes)
+        int d_q = d_r == L ? 0 : div(d_r,L).quot;
+
+        //D === I - Sp - difference between absolute values
+        //if D < 0 => we get already received packet that is out of the Out_p array
+        // -> 100% msg retransmit (no new info)
+        int D = d_q*2*L-d_r;
+
+        //check result
+        if(D != (I - Sp))
+        cout << D << "  vs  " << (I - Sp) << endl;
     }
 
     int a = 12345;
